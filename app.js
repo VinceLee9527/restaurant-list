d0e467a877f2c4e99dfe713fdf6cb17efe06b8ae
VinceLee9527 <vlee9527@gmail.com>
2021-08-18 21:14:53 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//express-handlebars
+const exphbs = require('express-handlebars')
+
+//template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//reuiqre json file with restaurant info
+const restaurantList = require('./restaurant.json')
+
+//static file set
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurant: restaurantList.results })
+})
+
+app.get('/search', (req, res) => {
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase())
+  })
+
+  res.render('index', { restaurant: restaurants, keyword: req.query.keyword })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+
+
+app.listen(port, () => {
+  console.log(`This server is now listening at localhost:${port}`)
+})
